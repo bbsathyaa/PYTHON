0fd7f822894a8da0ea012882ee1352b330c51131
bbsathyaa <74930612+bbsathyaa@users.noreply.github.com>
2020-11-24 22:30:41 +0530

    Add files via upload

---
+++ task 2(2).py
@@ -0,0 +1,4 @@
+Player = "India"
+print(len(Player))
+Player = Player.replace(Player[2],'G')
+print(Player)
---
+++ task 2(3).py
@@ -0,0 +1,8 @@
+a, b = 15, 2.5
+print(a, 'is in the type of:', type(a))
+a = float(a)
+print(a, 'is in the type of:', type(a))
+
+print(b, 'is in the type of:', type(b))
+b= int(b)
+print(b, 'is in the type of:', type(b))
