b4863f32c4e71a8c7b584ebe3b8adb986d8be059
bbsathyaa <bbsathyaabala08@gmail.com>
2020-12-06 16:45:04 +0530

    Add files via upload

---
+++ task  7.py
@@ -0,0 +1,21 @@
+
+
+from day7module import list_of_numbers
+import pandas as pd
+import random as rd
+
+import sys
+print(list_of_numbers)
+
+list_of_numbers[3] = 89
+
+print(list_of_numbers)
+
+a = rd.randint(1, 100)
+
+print(a)
+
+import sys
+
+for p in sys.path:
+    print(p)
